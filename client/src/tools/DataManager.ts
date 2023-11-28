7f0841280d14c506055b56d1fc71f0a63a523836
Christian Marcelo <christian.marcelo.castro@gmail.com>
2023-11-28 01:50:47 -0400

    challenge

--- client/src/tools/DataManager.ts
+++ client/src/tools/DataManager.ts
@@ -2,30 +2,32 @@ import { MongoClient, Db, Collection, FindCursor } from "mongodb";
 import { Technology } from "@/tools/data.model";
 
 // MongoDB constants
+console.log("MY_MONGO_URL:", process.env.MY_MONGO_URL);
 // const MONGO_URL:string = process.env.MONGO_URL!;
-const MONGO_URL:string = process.env.MONGO_URL || "mongodb://mongo:27017";
-const MONGO_DB_NAME:string = "dbTechs";	
-const MONGO_COLLECTION_TECHS:string = "technologies";
+// const MONGO_URL: string = process.env.MY_MONGO_URL || "mongodb://mongo:27017";
+const MONGO_URL: string = process.env.MY_MONGO_URL!;
+const MONGO_DB_NAME: string = "dbTechs";
+const MONGO_COLLECTION_TECHS: string = "technologies";
 
 export async function getTechnologies() {
     // construct a MongoClient object
     let mongoClient: MongoClient = new MongoClient(MONGO_URL);
 
-    let techArray:Technology[];
+    let techArray: Technology[];
     try {
         // make connection to mongoDB server (ASYNC task)
         await mongoClient.connect();
         // get reference to database via name
-        let db:Db = mongoClient.db(MONGO_DB_NAME);
+        let db: Db = mongoClient.db(MONGO_DB_NAME);
         // get reference to desired collection in DB
-        let collection:Collection<Technology> = db.collection<Technology>(MONGO_COLLECTION_TECHS);       
+        let collection: Collection<Technology> = db.collection<Technology>(MONGO_COLLECTION_TECHS);
         // get all documents of collection
-        let cursor:FindCursor = collection.find();
+        let cursor: FindCursor = collection.find();
         // get array of documents from cursor (ASYNC task)
         techArray = await cursor.toArray();
         // need to convert ObjectId objects to strings
-        techArray.forEach((tech:Technology) => tech._id = tech._id.toString());
-    } catch (error:any) {
+        techArray.forEach((tech: Technology) => tech._id = tech._id.toString());
+    } catch (error: any) {
         console.log(`>>> ERROR : ${error.message}`);
         throw error;
     } finally {
